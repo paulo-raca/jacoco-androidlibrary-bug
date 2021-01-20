10815fe0dcc07c0d6989404d2c5b491cb3c16c09
Paulo Costa <me@paulo.costa.nom.br>
2021-01-20 15:45:34 -0300

    Example reproduction project for bug on unittest in android libraries with jacoco enabled

---
+++ app/src/main/java/bugreport/jacocotest/Coverage.java
@@ -0,0 +1,11 @@
+package bugreport.jacocotest;
+
+public class Coverage {
+    public static void m1() {
+        System.out.println("m1");
+    }
+
+    public static void m2() {
+        System.out.println("m2");
+    }
+}
---
+++ app/src/test/java/bugreport/jacocotest/CoverageTest.java
@@ -0,0 +1,10 @@
+package bugreport.jacocotest;
+
+import org.junit.Test;
+
+public class CoverageTest {
+    @Test
+    public void m1() {
+        Coverage.m1();
+    }
+}
